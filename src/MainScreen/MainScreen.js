0c35113b4c5ff58f57921bb0bdf5971ca90a0f1c
Muhammad Rendra <muhammadnurrendra@gmail.com>
2021-08-09 09:05:10 +0700

    add 'onChange' on 'MainScreen.js'

--- src/MainScreen/MainScreen.js
+++ src/MainScreen/MainScreen.js
@@ -5,8 +5,10 @@ import { ResponsiveContext } from '@materialios/contexts'
 
 import './MainScreen.css'
 
-const MainScreen = ({ children }) => {
+const MainScreen = ({ children, onChange = () => {} }) => {
   const { orientation, device, width } = useContext(ResponsiveContext)
+
+  onChange({ orientation, device, width })
 
   const style = {
     width: `${orientation === 'Landscape' && device === 'Desktop' ? (width - 320) : width}px`
