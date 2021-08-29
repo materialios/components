b4cba6a8d40eb0437f0b90c70743d03cb1d02030
Muhammad Rendra <muhammadnurrendra@gmail.com>
2021-08-29 21:19:57 +0700

    add Container height

--- src/Container/Container.js
+++ src/Container/Container.js
@@ -30,17 +30,37 @@ const getPadding = (orientation, device) => {
   }
 }
 
-const Container = ({ children, withPadding = false, style = {} }) => {
-  const { orientation, device, size, width } = useContext(ResponsiveContext)
+const getHeight = (orientation, device, height) => {
+  if (orientation === 'Landscape') {
+    if (device === 'Handset') return height - 48
+    else if (device === 'Tablet') return height - 56
+    else if (device === 'Desktop') return height - 64
+  } else {
+    if (device === 'Handset') return height - 56
+    else if (device === 'Tablet') return height - 64
+    else if (device === 'Desktop') return height - 72
+  }
+}
+
+const Container = ({
+  children,
+  withPadding = false,
+  autoHeight = false,
+  style = {}
+}) => {
+  const { orientation, device, size, width, height } = useContext(ResponsiveContext)
 
   const mainScreenWidth = orientation === 'Landscape' && device === 'Desktop' ? (width - 320) : width
   const containerWidth = getWidth(orientation, device, size, mainScreenWidth)
   const padding = getPadding(orientation, device)
+  const containerHeight = getHeight(orientation, device, height)
 
   const thisStyle = {
     width: `${containerWidth}px`,
     margin: `0 ${(mainScreenWidth - containerWidth) / 2}px`,
-    padding: `${withPadding ? padding : 0}`
+    padding: `${withPadding ? padding : 0}`,
+    height: autoHeight ? 'auto' : containerHeight + 'px',
+    overflow: 'auto'
   }
 
   return (
