8799b17fae77685a84e67d3629f6d43a5e70d4ad
Muhammad Rendra <muhammadnurrendra@gmail.com>
2021-08-29 19:37:06 +0700

    fixing Col's className

--- src/Col/Col.js
+++ src/Col/Col.js
@@ -1,3 +1,7 @@
+import { useContext } from 'react'
+
+import { ResponsiveContext } from '@materialios/contexts'
+
 import './Col.css'
 
 const Col = ({
@@ -11,13 +15,15 @@ const Col = ({
   className = '',
   style = {}
 }) => {
+  const { orientation, device } = useContext(ResponsiveContext)
+
   let colClasses = ''
-  if (portraitHandset) colClasses = colClasses + ' Col-Portrait-Handset-' + portraitHandset
-  if (portraitTablet) colClasses = colClasses + ' Col-Portrait-Tablet-' + portraitTablet
-  if (portraitDesktop) colClasses = colClasses + ' Col-Portrait-Desktop-' + portraitDesktop
-  if (landscapeHandset) colClasses = colClasses + ' Col-Landscape-Handset-' + landscapeHandset
-  if (landscapeTablet) colClasses = colClasses + ' Col-Landscape-Tablet-' + landscapeTablet
-  if (landscapeDesktop) colClasses = colClasses + ' Col-Landscape-Desktop-' + landscapeDesktop
+  if (orientation === 'portrait' && device === 'Handset' && portraitHandset) colClasses = colClasses + ' Col-Portrait-Handset-' + portraitHandset
+  if (orientation === 'portrait' && device === 'Tablet' && portraitTablet) colClasses = colClasses + ' Col-Portrait-Tablet-' + portraitTablet
+  if (orientation === 'portrait' && device === 'Desktop' && portraitDesktop) colClasses = colClasses + ' Col-Portrait-Desktop-' + portraitDesktop
+  if (orientation === 'landscape' && device === 'Handset' && landscapeHandset) colClasses = colClasses + ' Col-Landscape-Handset-' + landscapeHandset
+  if (orientation === 'landscape' && device === 'Tablet' && landscapeTablet) colClasses = colClasses + ' Col-Landscape-Tablet-' + landscapeTablet
+  if (orientation === 'landscape' && device === 'Desktop' && landscapeDesktop) colClasses = colClasses + ' Col-Landscape-Desktop-' + landscapeDesktop
 
   return (
     <div className={`Col${colClasses ? ' ' + colClasses : ''}${className ? ' ' + className : ''}`} style={style}>
