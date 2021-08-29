9e437bad6259be0221dd66450053f1ab79e9091d
Muhammad Rendra <muhammadnurrendra@gmail.com>
2021-08-29 19:39:33 +0700

    fixing Col's className 2

--- src/Col/Col.js
+++ src/Col/Col.js
@@ -18,12 +18,12 @@ const Col = ({
   const { orientation, device } = useContext(ResponsiveContext)
 
   let colClasses = ''
-  if (orientation === 'portrait' && device === 'Handset' && portraitHandset) colClasses = colClasses + ' Col-Portrait-Handset-' + portraitHandset
-  if (orientation === 'portrait' && device === 'Tablet' && portraitTablet) colClasses = colClasses + ' Col-Portrait-Tablet-' + portraitTablet
-  if (orientation === 'portrait' && device === 'Desktop' && portraitDesktop) colClasses = colClasses + ' Col-Portrait-Desktop-' + portraitDesktop
-  if (orientation === 'landscape' && device === 'Handset' && landscapeHandset) colClasses = colClasses + ' Col-Landscape-Handset-' + landscapeHandset
-  if (orientation === 'landscape' && device === 'Tablet' && landscapeTablet) colClasses = colClasses + ' Col-Landscape-Tablet-' + landscapeTablet
-  if (orientation === 'landscape' && device === 'Desktop' && landscapeDesktop) colClasses = colClasses + ' Col-Landscape-Desktop-' + landscapeDesktop
+  if (orientation === 'Portrait' && device === 'Handset' && portraitHandset) colClasses = colClasses + ' Col-Portrait-Handset-' + portraitHandset
+  if (orientation === 'Portrait' && device === 'Tablet' && portraitTablet) colClasses = colClasses + ' Col-Portrait-Tablet-' + portraitTablet
+  if (orientation === 'Portrait' && device === 'Desktop' && portraitDesktop) colClasses = colClasses + ' Col-Portrait-Desktop-' + portraitDesktop
+  if (orientation === 'Landscape' && device === 'Handset' && landscapeHandset) colClasses = colClasses + ' Col-Landscape-Handset-' + landscapeHandset
+  if (orientation === 'Landscape' && device === 'Tablet' && landscapeTablet) colClasses = colClasses + ' Col-Landscape-Tablet-' + landscapeTablet
+  if (orientation === 'Landscape' && device === 'Desktop' && landscapeDesktop) colClasses = colClasses + ' Col-Landscape-Desktop-' + landscapeDesktop
 
   return (
     <div className={`Col${colClasses ? ' ' + colClasses : ''}${className ? ' ' + className : ''}`} style={style}>
