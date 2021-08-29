a9540382efb2debe49584bf5cdc52f279df0f692
Muhammad Rendra <muhammadnurrendra@gmail.com>
2021-08-30 01:17:06 +0700

    change Container width and padding

--- src/Container/Container.js
+++ src/Container/Container.js
@@ -4,31 +4,31 @@ import { ResponsiveContext } from '@materialios/contexts'
 
 import './Container.css'
 
-const getWidth = (orientation, device, size, width = 0) => {
+const getPadding = (orientation, device, size, width = 0, withPadding = false) => {
   if (orientation === 'Landscape') {
-    if (device === 'Handset') return width - 0 // (width - (2 x (0)))
+    if (device === 'Handset') return `8px ${withPadding ? 8 : 0}px`
     else {
-      if (width > 1120) return 1120 - 96 // (max-width - (2 x (24 + 24 + 0)))
-      else return width - 96 // (width - (2 x (24 + 24 + 12)))
+      if (width > 1120) return `12px ${withPadding ? ((width - 1120) / 2) + 60 : ((width - 1120) / 2) + 48}px`
+      else return `12px ${withPadding ? 60 : 48}px`
     }
   } else {
-    if (device === 'Handset') return width - 0 // (width - (2 x (0)))
+    if (device === 'Handset') return `8px ${withPadding ? 8 : 0}px`
     else {
-      if (size === 'Medium') return width - 0 // (width - (2 x (0)))
-      else return width - 96 // (width - (2 x (24 + 24 + 0)))
+      if (size === 'Medium') return `12px ${withPadding ? 60 : 48}px`
+      else return `12px ${withPadding ? ((width - 720) / 2) + 60 : ((width - 720) / 2) + 48}px`
     }
   }
 }
 
-const getPadding = (orientation, device) => {
-  if (orientation === 'Landscape') {
-    if (device === 'Handset') return '8px 12px'
-    else return '12px'
-  } else {
-    if (device === 'Handset') return '8px'
-    else return '12px'
-  }
-}
+// const getPadding = (orientation, device) => {
+//   if (orientation === 'Landscape') {
+//     if (device === 'Handset') return '8px 12px'
+//     else return '12px'
+//   } else {
+//     if (device === 'Handset') return '8px'
+//     else return '12px'
+//   }
+// }
 
 const getHeight = (orientation, device, height) => {
   if (orientation === 'Landscape') {
@@ -51,14 +51,17 @@ const Container = ({
   const { orientation, device, size, width, height } = useContext(ResponsiveContext)
 
   const mainScreenWidth = orientation === 'Landscape' && device === 'Desktop' ? (width - 320) : width
-  const containerWidth = getWidth(orientation, device, size, mainScreenWidth)
-  const padding = getPadding(orientation, device)
+  // const containerWidth = getWidth(orientation, device, size, mainScreenWidth)
+  // const padding = getPadding(orientation, device)
+  const padding = getPadding(orientation, device, size, mainScreenWidth, withPadding)
   const containerHeight = getHeight(orientation, device, height)
 
   const thisStyle = {
-    width: `${containerWidth}px`,
-    margin: `0 ${(mainScreenWidth - containerWidth) / 2}px`,
-    padding: `${withPadding ? padding : 0}`,
+    // width: `${containerWidth}px`,
+    width: `${mainScreenWidth}px`,
+    // margin: `0 ${(mainScreenWidth - containerWidth) / 2}px`,
+    // padding: `${withPadding ? padding : 0}`,
+    padding: padding,
     height: autoHeight ? 'auto' : containerHeight + 'px',
     overflow: 'auto'
   }
