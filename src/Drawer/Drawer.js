e48f3f1d3b3d45fdedbee29808b224d4066822c7
Muhammad Rendra <muhammadnurrendra@gmail.com>
2021-09-02 00:56:30 +0700

    update Drawer

--- src/Drawer/Drawer.js
+++ src/Drawer/Drawer.js
@@ -5,17 +5,17 @@ import { DrawerContext } from '@materialios/contexts'
 import './Drawer.css'
 
 const Drawer = ({ children }) => {
-  const [{ show }, setDrawer] = useContext(DrawerContext)
+  const { drawer, setDrawer } = useContext(DrawerContext)
 
   return (
-    <div className={`Drawer${show ? ' show' : ''}`}>
+    <div className={`Drawer${drawer.show ? ' show' : ''}`}>
       <div className='Drawer-Board'>
         <div className='Drawer-Board-Header'>{}</div>
         <div className='Drawer-Board-Body'>
           {children}
         </div>
       </div>
-      <div className='Drawer-Block' onClick={() => setDrawer({ show: !show })}>{}</div>
+      <div className='Drawer-Block' onClick={() => setDrawer({ show: !drawer.show })}>{}</div>
     </div>
   )
 }
